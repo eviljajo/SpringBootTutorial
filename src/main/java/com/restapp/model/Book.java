e7072e7bcd75255ed8be0d7b67a372d58fdb1c26
eviljajo <eviljajo@gmail.com>
2019-03-04 20:45:54 +0100

    First Commit

---
+++ src/main/java/com/restapp/model/Book.java
@@ -0,0 +1,24 @@
+package com.restapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+@Data
+@Entity
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false, unique = true)
+	private String title;
+	
+	@Column(nullable = false)
+	private String author;
+
+}
